da4a5477b2862aec746eec369677a52b7091a102
Gaurav Tupe <gauravtupe00@gmail.com>
2024-10-08 13:08:56 +0000

    updated the homepage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./HomePage.module.css";
+import { Link } from "react-router-dom";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className={styles.homePage}>
+      <h1 className={styles.homePageHeading}>Let"s Learn The Constitution</h1>
+      {
+        <>
+          <Link to="/lessons">
+            <button id={styles.playBtn}>Play</button>
+          </Link>
+          <Link to="/about">
+            {" "}
+            <button id={styles.aboutBtn}>&#8505;</button>
+          </Link>
+          <Link to="/settings">
+            <button id={styles.settingsBtn}>&#9881;</button>
+          </Link>
+        </>
+      }
+    </div>
+  );
+};
+export default HomePage;
